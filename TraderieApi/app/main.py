31670906538a0ba1eed8f302f6daab9ff63ce3ce
nadazelda_gram <nadazelda2@gmail.com>
2025-06-12 23:25:18 +0900

    no message

--- TraderieApi/app/main.py
+++ TraderieApi/app/main.py
@@ -9,7 +9,7 @@ from collections import Counter
 import json , os,re, glob
 from schemas.item import ItemRequest,ItemListRequest
 from services.url_builder import TraderieUrlBuilder
-from services.TerrorZoneFromD2Emu import TerrorZoneFromD2Emu
+from services.TerrorZon import TerrorZoneFromD2Emu
 from .kind_map import kind_map  # 같은 폴더에 있으면 이렇게 import
 from services.Crawler import Crawler  # 필요 시 상단으로 옮겨도 됨
 # main.py 또는 app.py에서
