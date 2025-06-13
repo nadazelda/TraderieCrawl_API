816c88b077c19a211a5da5d916b3946c28f40b03
nadazelda_gram <nadazelda2@gmail.com>
2025-06-14 00:04:59 +0900

    no message

--- TraderieApi/services/url_builder.py
+++ TraderieApi/services/url_builder.py
@@ -1,3 +1,5 @@
+from urllib.parse import quote
+
 class TraderieUrlBuilder:
     BASE_API_URL = "https://traderie.com/api/diablo2resurrected/listings"
     REAL_URL = "https://traderie.com/diablo2resurrected/product"
@@ -42,10 +44,23 @@ class TraderieUrlBuilder:
         if rarity:
             self.params["prop_Rarity"] = rarity
 
+    # def _build_query_string(self, exclude_keys=None):
+    #     if exclude_keys is None:
+    #         exclude_keys = []
+    #     return "&".join([f"{k}={v}" for k, v in self.params.items() if k not in exclude_keys])
+
+
     def _build_query_string(self, exclude_keys=None):
         if exclude_keys is None:
             exclude_keys = []
-        return "&".join([f"{k}={v}" for k, v in self.params.items() if k not in exclude_keys])
+        return "&".join([
+            f"{quote(str(k), safe='')}={quote(str(v), safe='')}"
+            for k, v in self.params.items()
+            if k not in exclude_keys
+        ])
+
+
+
 
     def get_base_url(self):
         return f"{self.BASE_API_URL}?{self._build_query_string()}"
